142ddcbe359bc96c91a89cb35bc09aae345d657e
Erik Flores <erikflores.code@gmail.com>
2024-05-19 17:12:21 -0400

    first commit

---
+++ app/src/main/java/com/example/mycoffee/ui/products/ProductsFragment.kt
@@ -0,0 +1,36 @@
+package com.example.mycoffee.ui.products
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.mycoffee.databinding.FragmentHoursBinding
+import com.example.mycoffee.databinding.FragmentProductsBinding
+
+class ProductsFragment : Fragment() {
+
+    private var _binding: FragmentProductsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProductsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
